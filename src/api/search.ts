536493619a6825ed382bcdb2ced5fdfe5aad44b9
LiaDAO <97869178+Han-Na-05-22@users.noreply.github.com>
2023-05-16 01:56:47 +0900

    feat: (#4) input, form style 수정, search api 및 dropdown 박스 추가

---
+++ src/api/search.ts
@@ -0,0 +1,23 @@
+import apiRequest from './index';
+
+// getSearch API data
+export const getSearchData = async (data: string, setData: any) => {
+  if (data.length === 0) {
+    return;
+  }
+  const getLocalData: any = localStorage.getItem(`${data}`);
+
+  if (getLocalData) {
+    setData(JSON.parse(getLocalData));
+  } else {
+    const response = await apiRequest
+      .get(`/search/?q=${data}&page=1&limit=10`)
+      .then((res: any) => {
+        localStorage.setItem(data, JSON.stringify(res.data.result));
+        return res.data.result;
+      })
+      .catch((err) => console.log('err', err));
+
+    setData(response);
+  }
+};
